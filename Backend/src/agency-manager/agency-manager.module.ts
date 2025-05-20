3da15b4dd637dd53fe279d1635bdf708cba8e936
GiuseppeSindoni <g.sindoni@studenti.unina.it>
2025-05-20 12:43:18 +0200

    aggiunta metody agency service

--- Backend/src/agency-manager/agency-manager.module.ts
+++ Backend/src/agency-manager/agency-manager.module.ts
@@ -1,19 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AgencyManagerController } from './agency-manager.controller';
-import { AgencyManagerService } from './agency-manager.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Manager } from './agency-manager.entity';
-import { SupportAdmin } from 'src/support-admin/support-admin.entity';
-import { User } from 'src/auth/user.entity';
-import { Agency } from 'src/agency/agency.entity';
-import { Agent } from 'src/agent/agent.entity';
-import { ConfigService } from '@nestjs/config';
 
-@Module({
-  controllers: [AgencyManagerController],
-  providers: [AgencyManagerService, ConfigService],
-  imports: [
-    TypeOrmModule.forFeature([Manager, User, SupportAdmin, Agency, Agent]),
-  ],
-})
+@Module({})
 export class AgencyManagerModule {}
