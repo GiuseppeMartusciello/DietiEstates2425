e8fe9a23325ee6307ff25ff96f49979649c9455c
Vincenzo <vincy22.vr@gmail.com>
2025-06-09 15:53:17 +0200

    Piccola modifica al servizio di notifica

--- Backend/src/notification/notification.service.ts
+++ Backend/src/notification/notification.service.ts
@@ -134,6 +134,8 @@ export class NotificationService {
   const notifications = await this.notificationRepository
     .createQueryBuilder('notification')
     .innerJoinAndSelect('notification.userNotifications', 'userNotification', 'userNotification.user.id = :userId', { userId })
+     .leftJoinAndSelect('notification.listing', 'listing')
+    .leftJoinAndSelect('notification.offer', 'offer')
     .orderBy('notification.date', 'DESC')
     .getMany();
 
